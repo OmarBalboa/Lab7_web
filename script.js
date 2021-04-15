54b7843c44826523fb62929d59c3fa1f50695249
Omar Balboa <omar_5425@hotmail.com>
2021-04-15 00:32:14 -0500

    se cambio el apikey utilizado y se modificaron lineas

--- script.js
+++ script.js
@@ -2,51 +2,47 @@ var temas = ['Soccer', 'Basketball', 'Football', 'Golf', 'Box', 'Tenis', 'UFC'];
 
 $(document).ready(function() {
     for (var i = 0; i < temas.length ; i++){
-        $("#animal-buttons").append("<button id= " + "ButtonTheme>" + temas[i] + "</button>");
+        $("#animal-buttons").append(`<button id= ButtonTheme>${temas[i]}</button>`);
     }
 });
 
 var api = "https://api.giphy.com/v1/gifs/search?";
-var apiKey = "&api_key=pijm5yVUARZfTZumrMwZob6s9a996b2d";
+var apiKey = "&api_key=aAZYxW8ThNhcV2OfLXhIjATDB2FANSdL";
 var limit = "&limit=10"
+
+var animal_in = "#animal-input";
+var animals = "#animals";
 
 $("#add-animal").on("click", function(event) {
     event.preventDefault();
-    if (!temas.includes($("#animal-input").val()) && $("#animal-input").val() != "" ){
-        $("#animal-buttons").append("<button id= " + "ButtonTheme>" +  $("#animal-input").val() + " </button>");
+    if (!temas.includes($(animal_in).val()) && $(animal_in).val() != "" ){
+        $("#animal-buttons").append(`<button id= ButtonTheme>${$("#animal-input").val()} </button>`);
     }
 
-    if ($("#animal-input").val() != "" ){
-        $("#animals").html("");
-        temas.push($("#animal-input").val());
-        var url = api + "&q=" + $("#animal-input").val() + apiKey + limit;
+    if ($(animal_in).val() != "" ){
+        $(animals).html("");
+        temas.push($(animal_in).val());
+        var url = api + "&q=" + $(animal_in).val() + apiKey + limit;
         $.get(url, function(giphy){
             for (var i = 0; i < giphy.data.length; i++){
-                $("#animals").append(`<div class= "animal-item"><p> rating: ${giphy.data[i].rating}</p><img src= ${giphy.data[i].images.fixed_height_still.url} static_url= ${giphy.data[i].images.fixed_height_still.url} moving_url= ${giphy.data[i].images.fixed_height.url} id="imgGiphy"></div>`);
+                $(animals).append(`<div class= "animal-item"><p> rating: ${giphy.data[i].rating}</p><img src= ${giphy.data[i].images.fixed_height_still.url} static_url= ${giphy.data[i].images.fixed_height_still.url} moving_url= ${giphy.data[i].images.fixed_height.url} id="imgGiphy"></div>`);
             }
         });
     }
 });
 
 $(document).on("click", "#imgGiphy", function(event) {
-    if ($(this).attr("moving_url") == $(this).attr("src")){
-        var staticUrl = $(this).attr("static_url");
-        $(this).attr("src", staticUrl);
-    }
-    else{
-        var moving = $(this).attr("moving_url");
-        $(this).attr("src", moving);
-    }
+    $(this).attr("moving_url") == $(this).attr("src") ? $(this).attr("src", $(this).attr("moving_url")) : $(this).attr("src", $(this).attr("static_url"));
 });
 
 
 $(document).on("click", "#ButtonTheme", function(event) {
     event.preventDefault();
-    $("#animals").html("");
+    $(animals).html("");
     var url = api + "&q=" + $(this).text() + apiKey + "&limit=10";
     $.get(url, function(giphy){
         for (var i = 0; i < giphy.data.length; i++){
-            $("#animals").append(`<div class= "animal-item"><p> rating: ${giphy.data[i].rating}</p><img src= ${giphy.data[i].images.fixed_height_still.url} static_url= ${giphy.data[i].images.fixed_height_still.url} moving_url= ${giphy.data[i].images.fixed_height.url} id="imgGiphy"></div>`);
+            $(animals).append(`<div class= "animal-item"><p> rating: ${giphy.data[i].rating}</p><img src= ${giphy.data[i].images.fixed_height_still.url} static_url= ${giphy.data[i].images.fixed_height_still.url} moving_url= ${giphy.data[i].images.fixed_height.url} id="imgGiphy"></div>`);
         }
     });
 });
